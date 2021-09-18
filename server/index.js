63976de997541d756225c308fa396cad3033c1ac
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-09-18 14:17:26 -0700

    setup db#3

--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
-import express from 'express';
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+require('dotenv').config();
+const key = process.env.MONGO_URL;
+
 
 const app = express();
 
 const PORT = 8080;
 
+
+// console.log(key);
+
+
+// mongoose.connect(process.env.MONGO_URL)
+// .then(()=>console.log('connected'))
+// .catch((e) => console.log(e))
+
+
+
+mongoose.connect('mongodb+srv://atwalsultan:atwalsultan@cluster0.xfj36.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(()=>console.log('connected'))
+.catch((e) => console.log(e))
+
 app.get('/', (req, res) => {
-    
+
     res.send("Hello");
 })
 
 app.listen(PORT, () => {
     console.log(`Server is ready at ${PORT}`);
 });
+
