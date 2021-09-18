a971baee40c080fc2f6986a2518e37ea11c1dca5
Sultan Atwal <sultan.singh.atwal@gmail.com>
2021-09-18 14:34:39 -0700

    #5 fix: change mongo uri and add env variable

--- server/index.js
+++ server/index.js
@@ -2,30 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 
-require('dotenv').config();
-const key = process.env.MONGO_URL;
-
+require('dotenv').config({path: 'server/.env'});
 
 const app = express();
 
 const PORT = 8080;
 
-
-// console.log(key);
-
-
-// mongoose.connect(process.env.MONGO_URL)
-// .then(()=>console.log('connected'))
-// .catch((e) => console.log(e))
-
-
-
-mongoose.connect('mongodb+srv://atwalsultan:atwalsultan@cluster0.xfj36.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log('connected'))
 .catch((e) => console.log(e))
 
 app.get('/', (req, res) => {
-
     res.send("Hello");
 })
 
